e107f4ce2b72e5b907fbdff48a5d10b019e7a07a
Mehul <mehulshrivastava1011@gmail.com>
2022-07-10 18:03:02 +0530

    On index.py used the sorted() function instead of sort(), much modular and we can use comparator function
    Comparator use is available

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
+import functools
+
 def sorting(arr, N):
-  # bubble sort
-  arr.sort()
-  
+  # instead using sorted() method
+  arr = sorted(arr, key= functools.cmp_to_key(compare))
+  print(arr)
+def compare(item1, item2):
+  if (item1 < item2):
+    return -1
+  else:
+    return 1
+
 if __name__ == "__main__":
   arr= [14, 1, 2, 12, 100, 1]
   N = len(arr)
