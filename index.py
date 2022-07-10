a135a14f89271962e0e378f438da7189d76e7432
Mehul Shrivastava <42713198+mehul1011@users.noreply.github.com>
2022-07-10 18:08:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,8 +6,10 @@ def sorting(arr, N):
 def compare(item1, item2):
   if (item1 < item2):
     return -1
-  else:
+  elif (item1 > item2):
     return 1
+  else:
+    return 0
 
 if __name__ == "__main__":
   arr= [14, 1, 2, 12, 100, 1]
